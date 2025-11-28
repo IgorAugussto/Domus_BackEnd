cb865a1493d95e47d6a32e10724e7a31bd02efbd
Igor Augusto <igor-augustofc@hotmail.com>
2025-11-28 02:44:39 -0300

    Subindo funcionalidade que vai calcular os investimentos cadastrados. Ainda falta alguns passos

---
+++ domus/src/main/java/com/igorAugusto/domus/domus/entity/Investments.java
@@ -0,0 +1,41 @@
+package com.igorAugusto.domus.domus.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "income")
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class Investments {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private BigDecimal value;  // ✅ Use BigDecimal para dinheiro!
+
+    @Column(nullable = false)
+    private String typeInvestments;  // Ex: "ITAUSA4"
+
+    @ManyToOne
+    @JoinColumn(name = "user_id", nullable = false)
+    private User user;  // ✅ Cada receita pertence a um usuário
+
+    @Column(name = "created_at", nullable = false)
+    private LocalDateTime createdAt;
+
+    @PrePersist
+    protected void onCreate() {
+        createdAt = LocalDateTime.now();
+    }
+}
