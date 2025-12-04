7d611fd0d701ab394cebf3948717792a80acd6d6
IgorAugussto <igor-augustofc@hotmail.com>
2025-12-04 18:00:45 -0300

    Corrigindo erro de login

--- domus/src/main/java/com/igorAugusto/domus/domus/config/SecurityConfig.java
+++ domus/src/main/java/com/igorAugusto/domus/domus/config/SecurityConfig.java
@@ -80,7 +80,7 @@ public class SecurityConfig {
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
-        configuration.setAllowedOriginPatterns(Arrays.asList("*"));           // permite qualquer origem (ou coloque "http://localhost:5173")
+        configuration.setAllowedOriginPatterns(Arrays.asList("http://localhost:5173"));           // permite qualquer origem (ou coloque "http://localhost:5173")
         configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "OPTIONS"));
         configuration.setAllowedHeaders(Arrays.asList("*"));
         configuration.setAllowCredentials(true); // importante se for usar cookies ou Authorization header
